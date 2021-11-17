a69d6490109a8efcb00b63114c08661c4cc32057
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-11-17 15:30:44 -0300

    Update BlogPessoal

--- BlogPessoal/src/main/java/com/example/demo/service/UsuarioService.java
+++ BlogPessoal/src/main/java/com/example/demo/service/UsuarioService.java
@@ -58,8 +58,7 @@ public class UsuarioService {
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
-				usuarioLogin.get()
-						.setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 
 				return usuarioLogin;
 
